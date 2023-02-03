ce13de2da4dc5fec08813b16a0304f711e3f9edb
Vinayak-Prasad <117915449+Vinayak-Prasad@users.noreply.github.com>
2023-02-03 09:41:03 +0530

    Add files via upload

---
+++ cie.java
@@ -0,0 +1,13 @@
+package cie;
+
+public class Internals {
+
+	public int internal[]=new int[5];
+}
+package cie;
+
+public class Student {
+	public String name;
+	public int usn;
+	public int sem;
+}
---
+++ see.java
@@ -0,0 +1,5 @@
+package see;
+import cie.Internals;
+public class External extends Internals {
+	public int external[]=new int[5];
+}
